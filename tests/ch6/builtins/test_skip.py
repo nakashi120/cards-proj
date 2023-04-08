c0494ded3e9f7aaa6f676c3cba4b27e38c78d9b8
nakashi120 <nakashi120@gmail.com>
2023-04-08 15:35:52 +0700

    Added equality test in ch5

--- tests/ch6/builtins/test_skip.py
+++ tests/ch6/builtins/test_skip.py
@@ -7,3 +7,9 @@ def test_less_than():
     c1 = Card("a task")
     c2 = Card("b task")
     assert c1 < c2
+
+
+def test_equality():
+    c1 = Card("a task")
+    c2 = Card("a task")
+    assert c1 == c2
